b4f1a001778e38f01cc7d67373309414c64db7c2
Artem Miller <artem.miller.dev@gmail.com>
2023-10-29 10:05:00 +0300

    Release 1.0 Beta

--- src/listbox.js
+++ src/listbox.js
@@ -1,7 +1,9 @@
 import './styles.css';
 import { getDuplicatesCount } from './utils';
 
-export class Listbox {
+export class Listbox {    
+    #currentSelectedIdx = -1;
+    
     /**
      * A listbox constructor.
      *
@@ -15,7 +17,8 @@ export class Listbox {
         this.options = options;
         this.theme = options.theme;
         
-        this.selectedItemID;
+        if (!this.theme) this.theme = 'light';
+
         this.styles = {};
     }
 
@@ -26,14 +29,15 @@ export class Listbox {
         this.styles = this.#getStyles();
         
         this.listbox = this.#element('div', ['listbox', this.styles.mainBGClass]);
+        this.listbox.tabIndex = 0;
         this.root.appendChild(this.listbox);
 
         this.items.forEach(item => this.add(item));
 
         this.listbox.addEventListener('click', this.listboxSelectHandler);
         this.listbox.addEventListener('dblclick', this.listboxDoubleClickHandler);
-        window.addEventListener('keydown', this.listboxSwitchItemByArrow);
-        window.addEventListener('keydown', this.unselectByESCHandler);
+        this.listbox.addEventListener('keyup', this.listboxSwitchItemByArrow);
+        this.listbox.addEventListener('keyup', this.keydownHandler);
     }
 
     // restart listbox.
@@ -46,8 +50,8 @@ export class Listbox {
     destroy() {
         this.listbox.removeEventListener('click', this.listboxSelectHandler);
         this.listbox.removeEventListener('dblclick', this.listboxDoubleClickHandler);
-        window.removeEventListener('keydown', this.unselectByESCHandler);
-        window.removeEventListener('keydown', this.listboxSwitchItemByArrow);
+        this.listbox.removeEventListener('keyup', this.keydownHandler);
+        this.listbox.removeEventListener('keyup', this.listboxSwitchItemByArrow);
         this.listbox.remove();
     }
 
@@ -58,7 +62,7 @@ export class Listbox {
      * @return {object} - all data of selected item.
      */
     select(id) {
-        const item = document.getElementById(id);
+        const item = this.listbox.querySelector('#' + id);
         return (item) ? {name: item.innerHTML, id: item.id} : {};
     }
 
@@ -84,13 +88,13 @@ export class Listbox {
     }
 
     /**
-     * Add item to listbox.
+     * Remove item from listbox.
      *
      * @param {string} id - item id.
      * @return {boolean} - function return true if item removed success else return false if such item isn't exists.
      */
     remove(id) {
-        const item = document.getElementById(id);
+        const item = this.listbox.querySelector('#' + id);
         
         if (item) {
             item.remove();
@@ -144,8 +148,10 @@ export class Listbox {
     listboxSelectHandler = (event) => {
         if (event.target.classList.contains('listbox__item')) {
             this.#unselectSelectedItems();
-            
-            //this.selectedItemID = event.target.id;
+
+            let currentItems = this.getAllItems();
+            const item = this.select(event.target.id);
+            this.#currentSelectedIdx = currentItems.findIndex(el => el.id === item.id);
 
             this.#setSelectColor(event.target);            
         }
@@ -154,28 +160,41 @@ export class Listbox {
     listboxDoubleClickHandler = (event) => {
         if (event.target.classList.contains('listbox__item')) {
             const id = event.target.id;
-            
-            if (this.options.onSelectCallback)
-                return this.options.onSelectCallback(this.select(id));
-            
-            console.log(this.select(id));
+            this.#selectedItemProcessing(id);
         }
     };
 
-    // function in development.
-    listboxSwitchItemByArrow(event) {
-        if (event.keyCode === 38) {
-            // up
-            
+    listboxSwitchItemByArrow = (event) => {
+        const items = this.getAllItems();
 
-        } else if (event.keyCode === 40) {
-            // down
+        if (event.key === 'ArrowUp') {
+            this.#currentSelectedIdx--;
+
+            if (this.#currentSelectedIdx < 0) this.#currentSelectedIdx = items.length - 1;
             
+            this.#unselectSelectedItems();
+            this.#setSelectColor(this.listbox.querySelector('#' + items[this.#currentSelectedIdx].id));
+
+        } else if (event.key === 'ArrowDown') {
+            this.#currentSelectedIdx++;
+
+            if (this.#currentSelectedIdx >= items.length) this.#currentSelectedIdx = 0;
+            
+            this.#unselectSelectedItems();
+            this.#setSelectColor(this.listbox.querySelector('#' + items[this.#currentSelectedIdx].id));
         }
     }
 
-    unselectByESCHandler = (event) => {
-        if (event.keyCode === 27) this.#unselectSelectedItems();
+    keydownHandler = (event) => {
+        if (event.key === 'Escape') { 
+            this.#unselectSelectedItems();
+            this.#currentSelectedIdx = -1;
+
+        } else if (event.key === 'Enter') {
+            if (this.#currentSelectedIdx < 0) return;
+            const items = this.getAllItems();
+            this.#selectedItemProcessing(items[this.#currentSelectedIdx].id);
+        }
     }
 
     // private methods
@@ -207,5 +226,11 @@ export class Listbox {
     #setSelectColor(target) {
         target.classList.add('listbox__item-selected');
         target.classList.remove(this.styles.itemHoverBG);
+    }
+
+    #selectedItemProcessing(id) {
+        if (this.options.onSelectCallback)
+            return this.options.onSelectCallback(this.select(id));
+        console.log(`Item with ID: ${id} selected`, this.select(id));
     }
 }
